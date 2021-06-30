24973ff7fabf8f770c35359df28bac6788deba7d
JayawardanaJTH <tiran2323@gmailcom>
2021-06-30 20:29:16 +0530

    Modified routes

--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router(); 
-const { addEvent, getEvents,getEvent,updateEvent,deleteEvent } = require('../controller/events')
+const { addEvent, getEvents,getEvent,getPendingEvents,getApprovedEvents,updateEvent,deleteEvent } = require('../controller/events')
 
 router.get('/event/:id', getEvent)
 router.get('/events', getEvents)
+router.get('/pending/events', getPendingEvents)
+router.get('/approved/events', getApprovedEvents)
 router.put('/event/:id', updateEvent)
 router.post('/event', addEvent)
 router.delete('/event/:id', deleteEvent)
--- routes/statics.js
+++ routes/statics.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router(); 
 const {upload} = require('../helpers/file-helper')
-const { addStatic,getStatics,updateStatic,deleteStatic } = require('../controller/statics')
+const { addStatic,getStatics,getPendingStatics,getApprovedStatics,updateStatic,deleteStatic } = require('../controller/statics')
 
 router.get('/statics', getStatics);
-router.put('/static/:id',upload.single('cover'), updateStatic);
+router.get('/pending/statics', getPendingStatics);
+router.get('/approved/statics', getApprovedStatics);
+router.put('/static/:id',upload.single('cover'), updateStatic); 
 router.post('/static', upload.single('cover'), addStatic);
 router.delete('/static/:id', deleteStatic);
 
